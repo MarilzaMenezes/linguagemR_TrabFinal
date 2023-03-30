3749b83d760e9b54f54fe052f327ef82e9a757eb
Marilza Menezes <112493404+MarilzaMenezes@users.noreply.github.com>
2023-03-30 18:59:51 -0300

    Add files via upload

---
+++ Projeto em Linguagem R.R
@@ -0,0 +1,13 @@
+library(nycflights13)
+library(dplyr)
+library(ggplot2)
+library(purrr)
+library (magrittr)
+library(stringr)
+library(tidyr)
+library(readr)
+
+
+dados <- read.csv("dataset_preco_gas_brasil.csv", header=TRUE)
+
+dados
